c61894ddc47a26364818924a430c8f8c5a0bf57a
itsnehagarg <nehagarg.94@gmail.com>
2022-03-25 23:41:14 -0600

    Add files via upload

---
+++ BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+	List<Book> findByNameContainsAndAuthorContainsAllIgnoreCase(String name,String author);
+}
